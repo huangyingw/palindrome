8f879e9b7265837a4ecce0b6a635d7fa4e7d4902
huangyingw@linux_programming <huangyingw@gmail.com>
2011-11-06 17:51:08 +0800

    n

--- palindrome.cc
+++ palindrome.cc
@@ -16,10 +16,22 @@ int palindrome_num(int num)
   return new_num == num;
 }
 
+int palindrome_str(char str[], int len)
+{
+  if (1==len)
+    return 1;
+  else if (str[len-1]!=str[0])
+    return 0;
+  else
+    return  palindrome_str(str+1,len-2);
+}
+
 int main() 
 {
   cout<<palindrome_num(12321)<<endl;
   cout<<palindrome_num(22321)<<endl;
   cout<<palindrome_num(123454321)<<endl;
+  cout<<palindrome_str("1234321",7)<<endl;
+  cout<<palindrome_str("1334321",7)<<endl;
   return 0;
 }
