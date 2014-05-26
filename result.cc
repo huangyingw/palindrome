f41970ecd4d93eaac6c59dbe60495b9970ec13d4
huangyingw@linux_programming <huangyingw@gmail.com>
2014-05-26 15:12:11 -0700

    n

---
+++ result.cc
@@ -0,0 +1,24 @@
+Buildfile: /media/volgrp/myproject/git/java/algorithm/palindrome/build.xml
+
+build-subprojects:
+
+init:
+
+build-project:
+     [echo] palindrome: /media/volgrp/myproject/git/java/algorithm/palindrome/build.xml
+
+build:
+
+PalindromeTest:
+    [junit] Running PalindromeTest
+    [junit] Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.057 sec
+    [junit] Error: 
+    [junit] log4j:WARN No appenders could be found for logger (PalindromeTest).
+    [junit] log4j:WARN Please initialize the log4j system properly.
+    [junit] log4j:WARN See http://logging.apache.org/log4j/1.2/faq.html#noconfig for more info.
+    [junit] 
+
+run:
+
+BUILD SUCCESSFUL
+Total time: 0 seconds
