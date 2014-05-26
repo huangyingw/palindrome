85b6ee4ee84668be266efd9e1ffb5884a2231f86
huangyingw@linux_programming <huangyingw@gmail.com>
2014-05-26 15:14:09 -0700

    n

--- result.cc
+++ result.cc
@@ -11,7 +11,7 @@ build:
 
 PalindromeTest:
     [junit] Running PalindromeTest
-    [junit] Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.058 sec
+    [junit] Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.057 sec
     [junit] Error: 
     [junit] log4j:WARN No appenders could be found for logger (PalindromeTest).
     [junit] log4j:WARN Please initialize the log4j system properly.
